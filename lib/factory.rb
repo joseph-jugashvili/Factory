aae3e24761f53b51ccef3d2a6521d434d18aab38
joseph-jugashvili <alexkv012407@gmail.com>
2021-11-11 11:58:20 +0200

    created Factory class

--- lib/factory.rb
+++ lib/factory.rb
@@ -14,3 +14,13 @@
 # - to_a
 # - values_at
 # - ==, eql?
+
+class Factory
+  def self.new(class_name, *args, &block)
+    if class_name.is_a? String
+      set_const(class_name, Struct.new(*args) {class_eval(&block) if block_given? })
+    else
+      Struct.new(*args.unshift(class_name)) { class_eval(&block) if block_given? }
+    end
+  end
+end

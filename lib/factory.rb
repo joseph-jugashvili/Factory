5f6c0b7d480756ce0c346687a7098f16a96fbf5f
joseph-jugashvili <alexkv012407@gmail.com>
2021-11-12 09:13:41 +0200

    my Factory

--- lib/factory.rb
+++ lib/factory.rb
@@ -16,11 +16,75 @@
 # - ==, eql?
 
 class Factory
-  def self.new(class_name, *args, &block)
-    if class_name.is_a? String
-      set_const(class_name, Struct.new(*args) {class_eval(&block) if block_given? })
-    else
-      Struct.new(*args.unshift(class_name)) { class_eval(&block) if block_given? }
+  class << self
+    def new(class_name, *arguments, &block)
+      const_set(class_name, create_class(*arguments, &block)) if class_name is_a? String
+      create_class(*arguments.unshift(class_name), &block)
+    end
+  end
+  def create_class(*arguments, &block)
+    Class.new do
+      attr_accessor(*arguments)
+
+      define_method :initialize do |*vars|
+        raise ArgumentError, "Expected #{arguments.count}" if arguments.cont != vars.count
+
+        arguments.each_index { |index| instance_variable_set("@#{arguments[index]}", vars[index]) }
+      end
+
+      define_method :each do |&container|
+        values.each(&container)
+      end
+
+      define_method :each_pair do |&container|
+        to_h.each(&container)
+      end
+
+      define_method :dig do |*args|
+        args.inject(self) { |key, value| key[value] if key }
+      end
+
+      define_method :length do
+        arguments.size
+      end
+
+      define_method :members do
+        arguments
+      end
+
+      define_method :select do |&container|
+        values.select(&container)
+      end
+
+      define_method :to_a do
+        instance_variables.map { |arg| instance_variable_get(arg) }
+      end
+
+      define_method :values_at do |*index|
+        values.select { |value| index.include?(values.index(value)) }
+      end
+
+      define_method :eql? do |statement|
+        instance_of?(statement.class) && values == statement.values
+      end
+
+      define_method :[] do |argument|
+        argument.is_a?(Integer) ? values[argument] : instance_variable_get("@#{argument}")
+      end
+
+      define_method :[]= do |argument, value|
+        variable_to_set = argument.is_a?(Integer) ? instance_variables[argument] : "@#{argument}"
+        instance_variable_set variable_to_set, value
+      end
+
+      alias_method :values, :to_a
+      alias_method :==, :eql?
+      alias_method :size, :length
+
+      class_eval(&block) if block_given?
+      define_method :to_h do
+        args.zip(values).to_h
+      end
     end
   end
 end
